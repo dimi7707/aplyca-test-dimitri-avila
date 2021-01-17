5430f11d2cfa389f7c8a7b5ef297e2ddd9fa2431
Dimitri Avila <davila@rootstack.com>
2021-01-17 15:17:22 -0500

    working on top-bar

--- components/layout/top-nav.vue
+++ components/layout/top-nav.vue
@@ -1,22 +1,29 @@
 <template>
   <div class="top-nav">
-    <a class="active" href="#home">Dashboard</a>
+    <div class="menu-options">
+      <img src="~/assets/images/menu-left.svg" alt="menu-symbol">
+      <a class="active" href="#home">Dashboard</a>
+    </div>
+    <div class="user-profile">
+      <img src="~/assets/images/generic-user-image.svg" alt="">
+      <span>Marlene Sasoeur</span>
+    </div>
   </div>
 </template>
-
-<script>
-export default {
-
-}
-</script>
 
 <style lang="scss">
 @import '~assets/styles/variables.scss';
 
-
 .top-nav {
   overflow: hidden;
   background-color: $white;
+  padding: .7em 2em .7em 2em;
+  display: flex;
+  justify-content: space-between;
+
+  .menu-options {
+    display: flex;
+  }
 
   a {
     float: left;
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
-    <h1>
-      Probando el sass
-    </h1>
+    <div class="user-actions-section">
+      <button class="btn-add-user">
+        <span class="plus-symbol">+</span>
+        Add new user
+      </button>
+    </div>
     <users-table :users="users" />
 
   </div>
@@ -37,5 +40,24 @@ export default {
 </script>
 
 <style lang="scss">
+@import '~/assets/styles/variables.scss';
+
+.user-actions-section {
+  margin-top: 1.5em;
+  margin-bottom: 1.5em;
+}
+
+.btn-add-user {
+  border: 0;
+  color: $white;
+  background-color: $color-primary-button;
+  border-radius: 8px;
+  padding: 1em 2.5em 1em 2.5em;
+  font-size: 14px;
+
+  .plus-symbol {
+    margin-right: 1em;
+  }
+}
 
 </style>
